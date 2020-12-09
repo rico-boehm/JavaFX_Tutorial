a384935107ea421f7ae56c9e6aa92f4158cd178d
Rico Böhm <35216571+rico0803@users.noreply.github.com>
2020-12-09 21:51:39 +0100

    Learned about css styling and buttons/mnemonics

--- src/MyApp.java
+++ src/MyApp.java
@@ -1,9 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -21,18 +20,18 @@ public class MyApp extends Application { //this is the actual application class 
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("First Window"); //window title
+        /*stage.setTitle("First Window"); //window title
         stage.show(); //makes window actually appear
         stage.setWidth(600); //width of window
         stage.setHeight(400); //height of window
         stage.setX(400); //x position of window
-        stage.setY(250); //y position of window
+        stage.setY(250); //y position of window*/
 
         Stage stage2 = new Stage(); // thats how we create a new stage (window)
-        stage2.setWidth(600);
-        stage2.setHeight(700);
-        stage2.setX(1000);
-        stage2.setY(150);
+        stage2.setWidth(700);
+        stage2.setHeight(400);
+        stage2.setX(350);
+        stage2.setY(100);
         stage2.setTitle("Second Window");
 
 
@@ -56,32 +55,37 @@ public class MyApp extends Application { //this is the actual application class 
 
         //Scenes
 
-        VBox parent = new VBox(); // layout manager; controls how layout looks; a stage can have multiple layout managers for different parts of the window
+        VBox root = new VBox(); // layout manager; controls how layout looks; a stage can have multiple layout managers for different parts of the window
 
-        Scene scene1 = new Scene(parent); // creates new scene
+        Scene scene1 = new Scene(root); // creates new scene
         stage2.setScene(scene1); // adds scene to stage
         scene1.setCursor(Cursor.CLOSED_HAND); //changes how cursor looks
+        scene1.getStylesheets().add("stylesheets/styles.css");
 
 
 
         //Labels and Images
 
-        ImageView image1 = new ImageView("https://www.france-pub.com/images/city-paris-600.jpg"); // new image with link from web
+        ImageView image1 = new ImageView("https://cdn.iconscout.com/icon/free/png-256/small-diamond-geometric-blue-38006.png"); // new image with link from web
         // now it is possible to change the properties of it with image1.
         //Image image = new Image("https://www.france-pub.com/images/city-paris-600.jpg"); // also possible to create new image object and then plug it into imageview instead of link
         //parent.getChildren().add(image1); // adds child node to parent
 
         Label label1 = new Label("This is a text label", image1); // creates new label; is child to "parent"; adds an image to that label
-        label1.setTextFill(Color.web("#675489"));
+        label1.setTextFill(Color.web("#918273"));
         label1.setFont(new Font("Cambria", 45));
         label1.setRotate(34);
-        parent.getChildren().add(label1); // adds child to parent (use .addAll(<controls>) if you want to add multiple
+
+        Label label2 = new Label("This is a second label");
+        label2.setId("special-label");
+
+        root.getChildren().addAll(label1,label2); // adds child to parent (use .addAll(<controls>) if you want to add multiple
 
 
 
         //Hyperlinks
 
-        Hyperlink link = new Hyperlink("Click here"); // creates new link
+        /*Hyperlink link = new Hyperlink("Click here"); // creates new link
         parent.getChildren().add(link); // adds to parent
         link.setOnAction(e -> { // sets what happens when clicked
             System.out.println("Was clicked");
@@ -94,7 +98,28 @@ public class MyApp extends Application { //this is the actual application class 
 
         newLink.setOnAction(e -> { // when link is clicked text changes
             text.setText("You clicked");
+        });*/
+
+
+
+        //Buttons
+
+        Button button = new Button("_Click");
+        button.setStyle("-fx-font-size: 35px");
+        button.setMinSize(10, 10);
+        button.setPrefSize(100, 100);
+        button.setOnAction(e -> {
+            label1.setText("Test");
         });
+        root.getChildren().addAll(button);
+
+
+
+        //Mnemonic
+
+        button.setMnemonicParsing(true); //basically a shortcut (alt + underlined character)
+
+
 
 
         stage2.show();
